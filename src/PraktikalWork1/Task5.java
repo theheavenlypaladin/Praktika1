778684e4a4110bfcd6d9b09a94f9eec554791894
Pracktica 1 <vashchilov.maks@mail.ru>
2026-09-22 03:01:12 +0300

    Выполнение задания 5

---
+++ src/PraktikalWork1/Task5.java
@@ -0,0 +1,10 @@
+package practicalwork1;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Аргумент " + i + ": " + args[i]);
+        }
+    }
+}
